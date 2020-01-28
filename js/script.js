3c08cc3a328ddb2038496a5b8d1ce093c1139d53
Jose Miguel Anover <josemiguelanover@Miggys-Macbook-Pro-2.local>
2020-01-28 13:04:39 +0800

    Added a function where you could copy/paste the gradient swatches

--- js/script.js
+++ js/script.js
@@ -36,24 +36,46 @@ $(document).ready(function(){
         $("#colorName").append(kulay.name);
         $("main").attr('style', "background: linear-gradient(45deg," + kulay.colors + ")");
         
-        
         var hex = '';
+        var hexCode = '';
         $.each( kulay.colors, function( key, val ) {
-            hex += '<span style="background:' + val + '">' + '</span>';
+            
+            // Creates the swatches and appends them to the DOM
+            hex += '<span style="background:' + val + '">' + '<input type="text" readonly class="tooltip" value="' + val + '">'+ '<p>'+ val +'</p>'+ '</span>';
         });
+        
+        // Append loop to #hex <div>
         $("#hex").append(hex);
+        
+        // Copy to clipboard function
+        $("#hex span").click(function(){
+            $(this).closest("#hex span").children("input.tooltip").select();
+            document.execCommand("copy");
+
+            // Change back after 1 second
+            var $this = $(this).closest("#hex span").children("p");
+            var originalText = $this.text();
+            $this.text('Copied!');
+            setTimeout(function() {
+            $this.text(originalText)
+            }, 1000);
+
+        });
     });
     
-    //Pull random spit facts YO
+    
+    /*
+    ================= SPIT FACTS ===================
+    */
+    
+    // Pull random spit facts YO
     $.getJSON("https://uselessfacts.jsph.pl/random.json?language=en", function(datum){
         var spitfact = datum.text;
         var src_url = datum.source_url;
         var src = datum.source;
         
         $("q em").append(spitfact);
-        $("p#source").append("<a href='" + src_url +"'>" + src + "</a>");
     });
-    
     
     
 });
